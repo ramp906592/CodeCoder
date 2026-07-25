21105fa486f8020d56b9e3a863b277e8e810dd94
Ram prakash jha <ramp906592@gmail.com>
2026-07-25 12:54:03 +0530

    ✅ Check If Array is Sorted — JAVA solution #1

---
+++ check-if-array-is-sorted/java1Solution..java
@@ -0,0 +1,11 @@
+class CodeCoder {
+    public boolean isSorted(int[] arr) {
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+           if(arr[i-1]>arr[i]){
+            return false;
+           }
+        }
+        return true;
+    }
+}
