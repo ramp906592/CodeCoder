72b30b4a405dfd91bf0bb56fcacbd6a80f1e9819
Ram prakash jha <ramp906592@gmail.com>
2026-08-01 11:12:20 +0530

    ✅ Move Zeroes — JAVA solution #1

---
+++ move-zeroes/java1Solution..java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] moveZeroes(int[] arr) {
+        int n =arr.length;
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+        return arr;
+    }
+}
